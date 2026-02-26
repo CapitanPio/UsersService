c579f6e4b6106fb8806df383172ff66d3ec8d42a
Claudio Espinoza Marinao <cespinoza.marina@gmail.com>
2026-02-25 21:21:51 -0300

    corrections

--- vue-project/src/api/users_service_queries.js
+++ vue-project/src/api/users_service_queries.js
@@ -12,50 +12,50 @@ export const register = (username, email, password, roleId = null) =>
     api.post('/api/auth/register', { username, email, password, ...(roleId !== null && { roleId }) })
 
 export const changePassword = (oldPassword, newPassword) =>
-    api.post('/api/auth/change-password', { oldPassword, newPassword })
+    api.post('/api/auth/users/change-password', { oldPassword, newPassword })
 
 export const getUsers = () =>
-    api.get('/api/auth/users')
+    api.get('/api/users')
 
 export const updateUser = (userId, userData) =>
-    api.put(`/api/auth/update/${userId}`, userData)
+    api.put(`/api/users/update/${userId}`, userData)
 
 export const deleteUser = (userId) =>
-    api.delete(`/api/auth/delete/${userId}`)
+    api.delete(`/api/users/delete/${userId}`)
 
 // Permissions management
 
 export const getAllPermissions = () =>
-    api.get('/api/auth/permissions')
+    api.get('/api/permissions')
 
-export const getPermission = (permissionId) =>
-    api.get(`/api/auth/permissions/${permissionId}`)
+export const getPermission = (permissionName) =>
+    api.get(`/api/permissions/${permissionName}`)
 
 export const createPermission = (permissionData) =>
-    api.post('/api/auth/permissions', permissionData)
+    api.post('/api/permissions', permissionData)
 
 export const updatePermission = (permissionId, newName) =>
-    api.put(`/api/auth/permissions/${permissionId}`, { newName })
+    api.put(`/api/permissions/${permissionId}`, { newName })
 
 export const deletePermission = (permissionId) =>
-    api.delete(`/api/auth/permissions/${permissionId}`)
+    api.delete(`/api/permissions/${permissionId}`)
 
 // Roles management
 
 export const getAllRoles = () =>
-    api.get('/api/auth/roles')
+    api.get('/api/roles')
 
 export const getRole = (roleName) =>
-    api.get(`/api/auth/roles/${roleName}`)
+    api.get(`/api/roles/${roleName}`)
 
 export const createRole = (roleData) =>
-    api.post('/api/auth/roles', roleData)
+    api.post('/api/roles', roleData)
 
 export const updateRole = (roleData) =>
-    api.put('/api/auth/roles', roleData)
+    api.put('/api/roles', roleData)
 
 export const deleteRole = (roleName) =>
-    api.delete(`/api/auth/roles/${roleName}`)
+    api.delete(`/api/roles/${roleName}`)
 
 export const checkAccess = (role, permissions) =>
-    api.post('/api/auth/check-access', { role, permissions })
+    api.post('/api/check-access', { role, permissions })
